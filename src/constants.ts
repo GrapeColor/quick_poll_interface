bd3ad83ef798c971dee1267aed866ad54141954f
GrapeColor <grapecolor@users.noreply.github.com>
2021-05-04 11:47:32 +0900

    Add environment PORT.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
-export const APP_LISTEN_PORT = Number(process.env['APP_LISTEN_PORT']) || 443;
+export const APP_LISTEN_PORT
+  = Number(process.env['APP_LISTEN_PORT'])
+    || Number(process.env['PORT'])
+    || 443;
 
 export const FAMILY_PASS_PHRASE = process.env['FAMILY_PASS_PHRASE'] || '';
